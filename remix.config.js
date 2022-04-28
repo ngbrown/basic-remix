25e94a7ddf89572bf75a55fb4d58f738f9642dd1
Nathan Brown <nathan@nkbrown.us>
2022-04-28 00:09:45 -0700

    create-remix from 2022-04-28

--- remix.config.js
+++ remix.config.js
@@ -2,10 +2,9 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: [".*"]
+  ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  // devServerPort: 8002
 };
